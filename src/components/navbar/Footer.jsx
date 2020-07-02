aa3792eee2fbc988456fa1e27fab6830b1c6117b
tomuhenry <tomusangehenry02@gmail.com>
2020-07-02 21:00:13 +0300

    setup react app with landing page

---
+++ src/components/navbar/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// fixed-bottom
+
+const Footer = () => {
+  return (
+      <footer className="page-footer bg-secondary">
+        <div className="footer-copyright text-center py-3 text-light">
+          © Wonya 2020
+        </div>
+      </footer>
+  );
+};
+
+export default Footer;
